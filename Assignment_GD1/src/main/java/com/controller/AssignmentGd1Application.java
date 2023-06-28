e2c4e32155f9fe6540a566082c1293fa19ceb59a
Tien Sang <57895319+bindo2512@users.noreply.github.com>
2023-06-28 11:23:28 +0700

    the first commit

---
+++ Assignment_GD1/src/main/java/com/controller/AssignmentGd1Application.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentGd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentGd1Application.class, args);
+	}
+
+}
