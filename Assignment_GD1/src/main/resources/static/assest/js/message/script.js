ff32187f3e692d69720c86b4158ef2d54fa242e3
Sandk1nG <eazy97889@gmail.com>
2023-12-09 17:02:08 +0700

    Dành cho tester

--- Assignment_GD1/src/main/resources/static/assest/js/message/script.js
+++ Assignment_GD1/src/main/resources/static/assest/js/message/script.js
@@ -1,13 +1,17 @@
 app.controller("chat-ctrl", function($http, $scope) {
 
+    
+    // the same can be done with plain javascript
+
 this.client = StreamChat.getInstance("wgft9kxdbtzt");
+const token2 = this.client.createToken('john', Math.floor(Date.now() / 1000) + (60 * 60));
 this.client.connectUser(
     {
         id: 'jlahey',
         name: 'Jim Lahey',
         image: 'https://i.imgur.com/fR9Jz14.png',
     },
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmVkLWR1c3QtOSJ9.2dRli43dWGfwyKTeOKew_OCelzsUiftv71QuFSOSx9Q",
+    client.devToken(token2),
 );
 
 })
